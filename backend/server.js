bc9c1c39de6930f5d8d981bae29fc5eae9daacbb
ki733655 <143171590+ki733655@users.noreply.github.com>
2025-08-22 19:08:42 +0530

    domain name correction

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ connectionToDatabase();
 
 // Middleware to parse JSON bodies
 app.use(cors({
-  origin: "https://trackmatee.vercel.app",
+  origin: "https://trackmatee.netlify.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
